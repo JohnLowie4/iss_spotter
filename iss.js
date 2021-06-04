005e50eb8f5c2dfbf3c14abfd4b4014b1dd66d46
John <jzlowie@gmail.com>
2021-06-03 20:51:21 -0600

    modified code to work with fetchCoordsByIP only

--- iss.js
+++ iss.js
@@ -1,34 +1,47 @@
 const request = require('request');
+// const url = 'https://freegeoip.app/json/invalidIPHere';
 const url = 'https://freegeoip.app/json/';
 
-const fetchMyIP = function(callback) {
-  request(url, (error, response, body) => {
-    if (error) return callback(error, null);
+// const fetchMyIP = function(callback) {
+//   request(url, (error, response, body) => {
+//     if (error) return callback(error, null);
 
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+
+//     // if (!error) {
+//     //   const ip = JSON.parse(body).ip;
+//     //   if (!ip) callback(null, ip);
+//     //   if (ip) callback(null, ip);
+//     //   return;
+//     // }
+
+//     const ip = JSON.parse(body).ip;
+//     callback(null, ip);
+
+//   });
+// };
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(url + ip, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
-
-    // if (!error) {
-    //   const ip = JSON.parse(body).ip;
-    //   if (!ip) callback(null, ip);
-    //   if (ip) callback(null, ip);
-    //   return;
-    // }
-
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
-
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
   });
 };
 
-const fetchCoordsByIP = function(ip, callback) {
-
-};
-
 module.exports = {
-  fetchMyIP,
+  // fetchMyIP,
   fetchCoordsByIP
 };
