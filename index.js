80a0c5011840293f8bf95aae63353cb5fb009705
John <jzlowie@gmail.com>
2021-06-03 22:33:00 -0600

    ISS Spotter III commit, something wrong with index.js example coordinates, only works with example in file

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // const { fetchMyIP, fetchCoordsByIP } = require("./iss");
-const { fetchCoordsByIP } = require("./iss");
+// const { fetchCoordsByIP } = require("./iss");
+const { fetchISSFlyOverTimes } = require("./iss");
+
+fetchISSFlyOverTimes({ latitude: 40.0, longitude: '-105.251945'}, (error, timeStamp) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned rise time and duration: ', timeStamp);
+});
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -10,10 +19,10 @@ const { fetchCoordsByIP } = require("./iss");
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIP('50.99.199.84', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned coordinations:', coordinates);
-});
+// fetchCoordsByIP('50.99.199.84', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinations:', coordinates);
+// });
--- iss.js
+++ iss.js
@@ -1,6 +1,39 @@
 const request = require('request');
-// const url = 'https://freegeoip.app/json/invalidIPHere';
-const url = 'https://freegeoip.app/json/';
+// const url = 'https://freegeoip.app/json/';
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `http://api.open-notify.org/iss/v1/?lat=${coords.latitude}.&lon=${coords.longitude}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching rise time and duration for ISS. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const time = JSON.parse(body).response;
+    callback(null, time);
+  });
+};
+
+// const fetchCoordsByIP = function(ip, callback) {
+//   request(url + ip, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//       return;
+//     }
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     }
+//     const { latitude, longitude } = JSON.parse(body);
+//     callback(null, { latitude, longitude });
+//   });
+// };
 
 // const fetchMyIP = function(callback) {
 //   request(url, (error, response, body) => {
@@ -12,36 +45,14 @@ const url = 'https://freegeoip.app/json/';
 //       return;
 //     }
 
-//     // if (!error) {
-//     //   const ip = JSON.parse(body).ip;
-//     //   if (!ip) callback(null, ip);
-//     //   if (ip) callback(null, ip);
-//     //   return;
-//     // }
-
 //     const ip = JSON.parse(body).ip;
 //     callback(null, ip);
 
 //   });
 // };
 
-const fetchCoordsByIP = function(ip, callback) {
-  request(url + ip, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    }
-    const { latitude, longitude } = JSON.parse(body);
-    callback(null, { latitude, longitude });
-  });
-};
-
 module.exports = {
   // fetchMyIP,
-  fetchCoordsByIP
+  // fetchCoordsByIP
+  fetchISSFlyOverTimes
 };
