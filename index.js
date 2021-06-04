a4c9e476b345888c0981b225b781da80899cea25
John <jzlowie@gmail.com>
2021-06-03 20:51:56 -0600

    modified code to work with fetchCoordsByIP only

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const { fetchMyIP, fetchCoordsByIP } = require("./iss")
+// const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchCoordsByIP } = require("./iss");
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('50.99.199.84', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
-  console.log('It worked! Returned IP:' , ip);
-});
-
-fetchCoordsByIP('50.99.199.84', (error, data) => {
-
+  console.log('It worked! Returned coordinations:', coordinates);
 });
